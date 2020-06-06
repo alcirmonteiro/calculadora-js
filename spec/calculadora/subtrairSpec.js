fb869191bc3790c2403a731f9d29998b734a3fd5
Alcir dos Santos Monteiro <alcirmonteiro@gmail.com>
2020-06-05 21:53:18 -0300

    Implementação da função de subtração

---
+++ spec/calculadora/subtrairSpec.js
@@ -0,0 +1,24 @@
+describe("Suite de testes de subtração", () => {
+	var Calculadora = require('../../src/js/Calculadora.js');
+
+	it('Deve retornar 5 para 8 e 3', () => {
+		expect(Calculadora.subtrair(8, 3)).toEqual(5);
+	});
+
+	it('Deve retornar 5 para 8 e 3 no formato texto', () =>{
+		expect(Calculadora.subtrair('8', '3')).toEqual(5);
+	});
+
+    it('Deve retornar 4 para 5.5 e 1.5', () =>{
+    	expect(Calculadora.subtrair(5.5, 1.5)).toEqual(4);
+    	expect(Calculadora.subtrair('5.5', '1.5')).toEqual(4);
+    });
+
+    it('Deve retornar 0 para valor 1 inválido', () =>{
+    	expect(Calculadora.subtrair(undefined, 10)).toEqual(0);
+    });
+
+    it('Deve retornar 0 para valor 2 inválido', () =>{
+    	expect(Calculadora.subtrair(10, undefined)).toEqual(0);
+    });
+})
--- src/js/calculadora.js
+++ src/js/calculadora.js
@@ -1,10 +1,6 @@
 var Calculadora = {
 	adicionar: (num1, num2) => {
-		if(isNaN(num1)){
-			return 0;
-		}
-
-		if(isNaN(num2)){
+		if(isNaN(num1) || isNaN(num2)){
 			return 0;
 		}
 
@@ -14,7 +10,12 @@ var Calculadora = {
 		return num1 + num2;
 	},
 	subtrair: (num1, num2) => {
-		return 0;
+		if(isNaN(num1) || isNaN(num2)){
+			return 0;
+		}
+
+
+		return num1 - num2;
 	},
 	dividir: (num1, num2) => {
 		return 0;
